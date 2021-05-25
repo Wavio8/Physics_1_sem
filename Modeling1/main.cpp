0744900d506b04d88652a6d17019e6d064b83388
Wavio8 <79695516+Wavio8@users.noreply.github.com>
2021-05-25 18:56:50 +0300

    Update main.cpp

--- Modeling1/main.cpp
+++ Modeling1/main.cpp
@@ -6,8 +6,10 @@ using namespace std;
 auto const G = 6.6743015 * pow(10, -11);
 
 void mass_change(int &m_oil, int v_oil, int &m_rocket) {
-    m_oil -= v_oil;
-    m_rocket -= v_oil;
+    if (m_oil != 0) {
+        m_oil -= v_oil;
+        m_rocket -= v_oil;
+    }
 }
 
 void radius_change(double &radius, double x, double y, double R_EARTH) {
@@ -18,9 +20,17 @@ void gravity_attraction_change(int m_earth, int m_rocket, double &gravity_pull, 
     gravity_pull = G * m_earth / pow(radius, 2) * m_rocket;
 }
 
-void speed_change(double &v_x, double &v_y, double gravity_pull, double angle, int m_rocket, int v_gas, int v_oil) {
-    v_x += sin(angle) * v_gas * v_oil / double(m_rocket);
-    v_y += cos(angle) * v_gas * v_oil / double(m_rocket);
+void speed_change(double &v_x, double &v_y, double gravity_pull, double &angle, int m_rocket, int v_gas, int v_oil,
+                  int m_oil) {
+    if (m_oil != 0) {
+        if (angle == 90) {
+            v_x += sin(angle) * v_gas * v_oil / double(m_rocket);
+            v_y += cos(angle) * v_gas * v_oil / double(m_rocket);
+        } else {
+            v_x += v_gas * v_oil / double(m_rocket);
+            v_y += v_gas * v_oil / double(m_rocket);
+        }
+    }
     v_y -= gravity_pull;
 }
 
@@ -63,17 +73,15 @@ int main() {
             cout << "Speed y: " << v_y << endl;
             cout << "Module speed_change : " << pow(pow(v_x, 2) + pow(v_y, 2), 0.5) << endl;
             cout << "Oil mass: " << m_oil << endl;
-            if (m_oil == 0)
-                break;
             time++;
             mass_change(m_oil, V_OIL, m_rocket);
             radius_change(radius, x, y, R_EARTH);
             gravity_attraction_change(M_EARTH, m_rocket, gravity_pull, radius);
-            speed_change(v_x, v_y, gravity_pull, angle, m_rocket, V_GAS, V_OIL);
+            speed_change(v_x, v_y, gravity_pull, angle, m_rocket, V_GAS, V_OIL, m_oil);
             angle_change(angle, v_x, v_y);
             coordinates_change(x, y, v_x, v_y);
         } else {
-            cout << "Oil - yes, Rocket - no";
+            cout << "Rocket was fallen";
             break;
         }
 
